21777b81e1df2169058192c11835fd13eb9895f4
Deryn Boscariol <derynb@rogers.com>
2024-06-11 13:34:27 -0400

    added all bartender functionality, need to debug json serialization

---
+++ PassionProject/Controllers/BartenderController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Diagnostics;
+using PassionProject.Models;
+using PassionProject.Models.ViewModels;
+
+namespace PassionProject.Controllers
+{
+    public class BartenderController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        static BartenderController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44307/api/");
+        }
+
+        //GET: bartender/List
+        public ActionResult List() 
+        {
+            string url = "BartenderData/ListBartenders";
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+
+            IEnumerable<BartenderDto> Bartenders = responseMessage.Content.ReadAsAsync<IEnumerable<BartenderDto>>().Result;
+
+            return View(Bartenders);
+        }
+
+        //Get: Bartender/Details/id
+        public ActionResult Details(int id)
+        {
+            DetailsBartender ViewModel = new DetailsBartender();
+
+            string url = "bartenderData/FindBartender/" + id;
+            HttpResponseMessage ResponseMessage = client.GetAsync(url).Result;
+
+            BartenderDto SelectedBartender = ResponseMessage.Content.ReadAsAsync<BartenderDto>().Result;
+
+            ViewModel.SelectedBartender = SelectedBartender;
+
+            url = "cocktaildata/ListCocktailsByBartender/" + id;
+            ResponseMessage = client.GetAsync(url).Result;
+
+            IEnumerable<CocktailDto> CocktailsMade = ResponseMessage.Content.ReadAsAsync<IEnumerable<CocktailDto>>().Result;
+
+            ViewModel.CocktailsMade = CocktailsMade;
+
+            return View(ViewModel);
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+
+            return View(); 
+        }
+
+        //POST: bartenders/create
+        [HttpPost]
+        public ActionResult Create(Bartender Bartender)
+        {
+            Debug.WriteLine("the json payload is :");
+            Debug.WriteLine(Bartender.firstName + Bartender.lastName);
+
+            string url = "bartenderdata/addBartender";
+
+            string jsonpayload = serializer.Serialize(Bartender);
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        //GET: bartender/edit/id
+        public ActionResult Edit(int id)
+        {
+            string url = "bartenderdata/findbartender/" + id;
+            HttpResponseMessage ResponseMessage = client.GetAsync(url).Result;
+            BartenderDto SelectedBartender = ResponseMessage.Content.ReadAsAsync<BartenderDto>().Result;
+            return View(SelectedBartender);
+        }
+
+        //POST: bartender/update/id
+        public ActionResult Update(int id, Bartender Bartender) 
+        {
+            string url = "bartenderdata/updateBartender/" + id;
+            string jsonpayload = serializer.Serialize(Bartender);
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+            Debug.WriteLine(content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        //GET: bartender/delete/id
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "bartenderData/findbartender/" + id;
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+            BartenderDto SelectedBartender = responseMessage.Content.ReadAsAsync<BartenderDto>().Result;
+            return View(SelectedBartender);
+        }
+        // POST: Bartender/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "bartenderdata/deleteBartender/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ PassionProject/Controllers/BartenderDataController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PassionProject.Models;
+using System.Diagnostics;
+
+
+namespace PassionProject.Controllers
+{
+    public class BartenderDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //GET: api/bartenderdata/listbartenders
+        [HttpGet]
+        [ResponseType(typeof(BartenderDto))]
+        public IHttpActionResult ListBartenders()
+        {
+            List<Bartender> Bartenders = db.Bartenders.ToList();
+            List<BartenderDto> BartenderDtos = new List<BartenderDto>();
+
+            Bartenders.ForEach(b => BartenderDtos.Add(new BartenderDto()
+            {
+                bartenderId = b.bartenderId,
+                firstName = b.firstName,
+                lastName = b.lastName,
+                email = b.email,
+                numDrinks = b.numDrinks,
+                lastDrink = b.lastDrink
+            }));
+
+            return Ok(BartenderDtos);
+        }
+
+        //GET: api/BartenderData/FindBartender/id
+        [HttpGet]
+        [ResponseType(typeof(BartenderDto))]
+        public IHttpActionResult FindBartender(int id) 
+        {
+            Bartender Bartender = db.Bartenders.Find(id);
+            BartenderDto BartenderDto = new BartenderDto()
+            {
+                bartenderId = Bartender.bartenderId,
+                firstName = Bartender.firstName,
+                lastName = Bartender.lastName,
+                email = Bartender.email,
+                numDrinks = Bartender.numDrinks,
+                lastDrink = Bartender.lastDrink
+            };
+
+            if(Bartender == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(BartenderDto);
+        }
+
+        //POST: api/bartenderdata/updateBartender/id
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult UpdateBartender(int id, Bartender b)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != b.bartenderId)
+            {
+
+                return BadRequest();
+            }
+
+            db.Entry(b).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BartenderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        //POST: api/bartenderdata/addbartender
+        //FORM DATA: Bartender JSON object
+        [HttpPost]
+        [ResponseType(typeof(Bartender))]
+        public IHttpActionResult AddBartender(Bartender Bartender)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Bartenders.Add(Bartender);
+            db.SaveChanges();
+            return CreatedAtRoute("DefaultApi", new { id = Bartender.bartenderId }, Bartender);
+        }
+
+        //POST: api/BartenderData/DeleteBartender/id
+        //FORM DATA (empty)
+        [HttpPost]
+        [ResponseType(typeof(Bartender))]
+        public IHttpActionResult DeleteBartender(int id)
+        {
+            Bartender Bartender = db.Bartenders.Find(id);
+            if(Bartender == null)
+            {
+                return NotFound();
+            }
+            db.Bartenders.Remove(Bartender);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BartenderExists(int id)
+        {
+            return db.Bartenders.Count(b => b.bartenderId == id) > 0;
+        }
+    }
+}
