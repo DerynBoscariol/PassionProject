6e775dccef4500a137202b21b707fd75b1ccbcaf
Deryn Boscariol <derynb@rogers.com>
2024-06-10 21:33:26 -0400

    Flipped all bartener functionality to cocktails to better match one to many relationship

---
+++ PassionProject/Controllers/CocktailController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+using PassionProject.Models;
+using System.Security.Cryptography.X509Certificates;
+using PassionProject.Models.ViewModels;
+
+namespace PassionProject.Controllers
+{
+    public class CocktailController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        static CocktailController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                UseCookies = false
+            };
+
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44307/api/");
+        }
+
+        //GET: Cocktail/List
+        public ActionResult List()
+        {
+            //communicate with cocktail data controller to retrieve list of cocktails
+
+            string url = "CocktailData/ListCocktails";
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+
+            Debug.WriteLine("Response code: ");
+            Debug.WriteLine(responseMessage.StatusCode);
+
+            IEnumerable<CocktailDto> cocktails = responseMessage.Content.ReadAsAsync<IEnumerable<CocktailDto>>().Result;
+            Debug.WriteLine("Number of cocktails: ");
+            Debug.WriteLine(cocktails.Count());
+
+            return View(cocktails);
+        }
+
+        //GET: Cocktail/Details/id
+
+        public ActionResult Details(int id)
+        {
+            DetailsCocktail ViewModel = new DetailsCocktail();
+            //communicate with cocktail data controller to retrieve all information about 1 cocktail
+
+            string url = "CocktailData/FindCocktail/" + id;
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+
+            Debug.WriteLine("Response code: ");
+            Debug.WriteLine(responseMessage.StatusCode);
+
+            CocktailDto SelectedCocktail = responseMessage.Content.ReadAsAsync<CocktailDto>().Result;
+            Debug.WriteLine("Cocktail received: ");
+            Debug.WriteLine(SelectedCocktail.drinkName);
+
+            ViewModel.SelectedCocktail = SelectedCocktail;
+
+            //Show bartender who made the drink
+
+            url = "BartenderData/FindBartender/" + id;
+            responseMessage = client.GetAsync(url).Result;
+            IEnumerable<BartenderDto> BartenderCreated = responseMessage.Content.ReadAsAsync<IEnumerable<BartenderDto>>().Result;
+
+            ViewModel.BartenderCreated = BartenderCreated;
+
+            return View(ViewModel);
+
+        }
+
+        //POST: drink/Associate
+        [HttpPost]
+        public ActionResult Associate(int id, int bartenderId)
+        {
+            string url = "cocktaildata/Associate/"+ id + "/" +bartenderId;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("Details/" + id);
+        }
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        //POST: Cocktail/Create
+        [HttpPost]
+        public ActionResult Create(Cocktail cocktail)
+        {
+            Debug.WriteLine("Json payload: ");
+            Debug.WriteLine(cocktail.drinkId + " " + cocktail.drinkName);
+
+            string url = "AddCocktail";
+
+            string jsonpayload = serializer.Serialize(cocktail);
+
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        //GET: cocktail/edit/id
+        public ActionResult Edit(int id)
+        {
+            string url = "findCocktail/" + id;
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+
+            Debug.WriteLine("Response code: ");
+            Debug.WriteLine(responseMessage.StatusCode);
+
+            CocktailDto selectedCocktail = responseMessage.Content.ReadAsAsync<CocktailDto>().Result;
+
+            Debug.WriteLine("Cocktail received: ");
+            Debug.WriteLine(selectedCocktail.drinkId + " " + selectedCocktail.drinkName);
+
+            return View(selectedCocktail);
+        }
+
+        //POST: Cocktail/Update/id
+        [HttpPost]
+        public ActionResult Update(int id, Cocktail cocktail)
+        {
+            try
+            {
+                Debug.WriteLine("The new cocktail info is:");
+                Debug.WriteLine("Name: " + cocktail.drinkName);
+                Debug.WriteLine("Type: " + cocktail.drinkType);
+                Debug.WriteLine("Recipe: " + cocktail.drinkRecipe);
+                Debug.WriteLine("Alcoholic Ingredients: " + cocktail.liqIn);
+                Debug.WriteLine("Mix Ingredients: " + cocktail.mixIn);
+
+                string url = "UpdateCocktail/" + id;
+                string jsonpayload = serializer.Serialize(cocktail);
+                Debug.WriteLine(jsonpayload);
+
+                HttpContent content = new StringContent(jsonpayload);
+                content.Headers.ContentType.MediaType = "application/json";
+
+                //POST: api/CocktailData/UpdateCocktail/{id}
+                //Header : Content-Type: application/json
+                //
+                HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+
+                return RedirectToAction("Details/" + id);
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        //GET: Cocktail/Delete/id
+        public ActionResult Delete(int id, FormCollection collection) 
+        {
+           string url = "cocktailData/DeleteCocktail/" + id;
+           HttpContent content = new StringContent("");
+           content.Headers.ContentType.MediaType = "application/json";
+           HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+
+             if (responseMessage.IsSuccessStatusCode)
+             {
+              return RedirectToAction("List");
+             }
+              else
+            {
+              return RedirectToAction("Error");
+            }
+                
+        }
+    }
+}
--- PassionProject/Controllers/CocktailDataController.cs
+++ PassionProject/Controllers/CocktailDataController.cs
@@ -1,42 +1,209 @@
 ﻿using PassionProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+using System.Diagnostics;
 
 namespace PassionProject.Controllers
 {
     public class CocktailDataController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
+        /// <summary>
+        /// Returns a list of cocktails in the system
+        /// </summary>
+        /// <returns>Header 200 (OK)
+        /// Content: all cocktails in the database</returns>
+        /// <example>
+        /// GET: /api/CocktailData/ListCocktails ->
+        /// [{"drinkId" : 3, "firstName" : "Alex", "lastName" : "Turner" , "email" : "alexturner@example.com" , "numDrinks" : 4 "lastDrinkPosted" : 2020-07-21},
+        /// {"bartenderId" : 4, "firstName" : "Noah", "lastName" : "Kahan" , "email" : "noahkahan@example.com" , "numDrinks" : 3 "lastDrinkPosted" : 2024-03-18}]
+        /// </example>
         [HttpGet]
-        [Route("api/CocktailData/ListCocktails")]
-
-        public List<CocktailDto> ListCocktails()
+        [ResponseType(typeof(CocktailDto))]
+        public IHttpActionResult ListCocktails()
         {
             List<Cocktail> Cocktails = db.Cocktails.ToList();
 
-            List<CocktailDto> cocktailDtos = new List<CocktailDto>();
+            List<CocktailDto> CocktailDtos = new List<CocktailDto>();
 
-            foreach (Cocktail Cocktail in Cocktails)
+
+            Cocktails.ForEach(c => CocktailDtos.Add(new CocktailDto()
             {
-                CocktailDto CocktailDto = new CocktailDto();
-                CocktailDto.drinkId = Cocktail.drinkId;
-                CocktailDto.drinkName = Cocktail.drinkName;
-                CocktailDto.drinkType = Cocktail.drinkType;
-                CocktailDto.drinkRecipe = Cocktail.drinkRecipe;
-                CocktailDto.liqIn = Cocktail.liqIn;
-                CocktailDto.mixIn = Cocktail.mixIn;
-                CocktailDto.datePosted = Cocktail.datePosted;
-                CocktailDto.firstName = Cocktail.Bartender.firstName;
-                CocktailDto.lastName = Cocktail.Bartender.lastName;
+                drinkId = c.drinkId,
+                drinkName = c.drinkName,
+                drinkType = c.drinkType,
+                drinkRecipe = c.drinkRecipe,
+                liqIn = c.liqIn,
+                mixIn = c.mixIn,
+                datePosted = c.datePosted,
+                bartenderId = c.bartenderId
+            }));
 
-                cocktailDtos.Add(CocktailDto);
-            }
-            return cocktailDtos;
+            return Ok(CocktailDtos);
         }
+
+        /// <summary>
+        /// Gathers information about all Cocktails related to a specific bartenderId
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: all cocktails in database, including their bartenders matched with a specific bartenderId
+        /// </returns>
+        /// <param name="id">bartenderId</param>
+        /// <example>
+        /// GET: api/CocktailData/ListCocktailsByBartender/2
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(CocktailDto))]
+        public IHttpActionResult ListCocktailsByBartender(int id)
+        {
+            List<Cocktail> Cocktails = db.Cocktails.Where(b => b.bartenderId == id).ToList();
+            List<CocktailDto> CocktailDtos = new List<CocktailDto>();
+
+            Cocktails.ForEach(c => CocktailDtos.Add(new CocktailDto()
+            {
+                drinkId = c.drinkId,
+                drinkName = c.drinkName,
+                drinkType = c.drinkType,
+                drinkRecipe = c.drinkRecipe,
+                liqIn = c.liqIn,
+                mixIn = c.mixIn,
+                datePosted = c.datePosted,
+                bartenderId = c.bartenderId
+            }));
+
+            return Ok(CocktailDtos);
+        }
+
+        [ResponseType(typeof(Cocktail))]
+        [HttpGet]
+        [Route("api/CocktailData/FindCocktail/{id}")]
+        public IHttpActionResult FindCocktail(int id)
+        {
+            Cocktail Cocktail = db.Cocktails.Find(id);
+            CocktailDto CocktailDto = new CocktailDto()
+            {
+                drinkId = Cocktail.drinkId,
+                drinkName = Cocktail.drinkName,
+                drinkType = Cocktail.drinkType,
+                drinkRecipe = Cocktail.drinkRecipe,
+                liqIn = Cocktail.liqIn,
+                mixIn = Cocktail.mixIn,
+                datePosted = Cocktail.datePosted,
+                bartenderId = Cocktail.bartenderId
+            };
+
+            if (Cocktail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(CocktailDto);
+
+        }
+
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateCocktail(int id, Cocktail cocktail)
+        {
+            Debug.WriteLine("I have succesfully reached update cocktail method!");
+
+            if (!ModelState.IsValid)
+            {
+                Debug.WriteLine("Model State is invalid");
+                return BadRequest(ModelState);
+            }
+
+            if (id != cocktail.drinkId)
+            {
+                Debug.WriteLine("ID mismatch");
+                Debug.WriteLine("Get parameter" + id);
+                Debug.WriteLine("POST parameter" + cocktail.drinkId);
+                Debug.WriteLine("POST parameter" + cocktail.drinkName);
+                return BadRequest();
+            }
+
+            db.Entry(cocktail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+
+            {
+                if(!CocktailExists(id))
+                {
+                    Debug.WriteLine("Cocktail Not Found");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            Debug.WriteLine("No conditions triggered");
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+        // POST: api/CocktailData/AddCocktail
+        [ResponseType(typeof(Cocktail))]
+        [HttpPost]
+        public IHttpActionResult AddCocktail(Cocktail cocktail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cocktails.Add(cocktail);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        // POST: api/CocktailData/DeleteCocktail
+
+        [ResponseType(typeof(Cocktail))]
+        [HttpPost]
+        public IHttpActionResult DeleteCocktail(int id)
+        {
+            Cocktail cocktail = db.Cocktails.Find(id);
+
+            if (cocktail == null)
+            {
+                return NotFound();
+            }
+
+            db.Cocktails.Remove(cocktail);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CocktailExists(int id) 
+        {
+            return db.Cocktails.Count(e => e.drinkId == id) > 0;
+        }
+
     }
 }
